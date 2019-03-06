2e5b6972e21f2a7ca5d1bc0a096ff82afe0e66ac
Rene Martinez <rene.martinez.b86@gmail.com>
2019-03-06 16:48:28 -0300

    File upload

--- src/main/webapp/app/entities/discount-process/discount-process-update.component.ts
+++ src/main/webapp/app/entities/discount-process/discount-process-update.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpResponse, HttpEventType, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import * as moment from 'moment';
@@ -22,6 +22,14 @@ export class DiscountProcessUpdateComponent implements OnInit {
     environments: IEnvironment[];
     dateToProcess: string;
     createdDate: string;
+
+    @Input()
+    fileUpload: string;
+    showFile = false;
+    fileUploads: Observable<string[]>;
+    selectedFiles: FileList;
+    currentFileUpload: File;
+    progress: { percentage: number } = { percentage: 0 };
 
     constructor(
         protected jhiAlertService: JhiAlertService,
@@ -55,11 +63,39 @@ export class DiscountProcessUpdateComponent implements OnInit {
         this.isSaving = true;
         this.discountProcess.dateToProcess = this.dateToProcess != null ? moment(this.dateToProcess, DATE_FORMAT) : null;
         this.discountProcess.createdDate = this.createdDate != null ? moment(this.createdDate, DATE_TIME_FORMAT) : null;
+        this.discountProcess.sqlFilePath = this.currentFileUpload.name;
         if (this.discountProcess.id !== undefined) {
             this.subscribeToSaveResponse(this.discountProcessService.update(this.discountProcess));
         } else {
             this.subscribeToSaveResponse(this.discountProcessService.create(this.discountProcess));
         }
+    }
+
+    showFiles(enable: boolean) {
+        this.showFile = enable;
+
+        if (enable) {
+            this.fileUploads = this.discountProcessService.getFiles();
+        }
+    }
+
+    selectFile(event) {
+        this.selectedFiles = event.target.files;
+    }
+
+    upload() {
+        this.progress.percentage = 0;
+
+        this.currentFileUpload = this.selectedFiles.item(0);
+        this.discountProcessService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
+            if (event.type === HttpEventType.UploadProgress) {
+                this.progress.percentage = Math.round((100 * event.loaded) / event.total);
+            } else if (event instanceof HttpResponse) {
+                console.log('File is completely uploaded!');
+            }
+        });
+
+        this.selectedFiles = undefined;
     }
 
     protected subscribeToSaveResponse(result: Observable<HttpResponse<IDiscountProcess>>) {
